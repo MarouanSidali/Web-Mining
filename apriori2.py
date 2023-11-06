8f33bfd426b56724341c1737bff304690831228e
MarouanSidali <marwandx57@gmal.com>
2023-11-06 14:03:43 +0100

    First commit

---
+++ apriori2.py
@@ -0,0 +1,148 @@
+# Load the transactions from the dataset
+from itertools import combinations
+
+def load_data(filename):
+    data = []
+    with open(filename, 'r') as file:
+        for line in file:
+            transaction = line.strip().split(',')
+            data.append(transaction)
+    return data
+
+# Generate frequent itemsets of size 1
+def get_frequent_1_itemsets(data, min_support):
+    item_counts = {}
+    for transaction in data:
+        for item in transaction:
+            if item in item_counts:
+                item_counts[item] += 1
+            else:
+                item_counts[item] = 1
+
+    frequent_1_itemsets = {item: support for item, support in item_counts.items() if support >= min_support}
+    return frequent_1_itemsets
+
+# Generate candidate itemsets of size k from frequent itemsets of size k-1
+def generate_candidates(prev_frequent_itemsets, k):
+    candidates = set()
+    for itemset1 in prev_frequent_itemsets:
+        for itemset2 in prev_frequent_itemsets:
+            itemset1 = set(itemset1)
+            itemset2 = set(itemset2)
+            if len(itemset1.union(itemset2)) == k:
+                candidates.add(tuple(sorted(itemset1.union(itemset2))))
+    return candidates
+
+# Prune candidate itemsets that have infrequent subsets
+def prune_candidates(candidates, prev_frequent_itemsets, k):
+    pruned_candidates = set()
+    for candidate in candidates:
+        is_frequent = True
+        subsets = [set(item) for item in combinations(candidate, k - 1)]
+        for subset in subsets:
+            if tuple(sorted(subset)) not in prev_frequent_itemsets:
+                is_frequent = False
+                break
+        if is_frequent:
+            pruned_candidates.add(candidate)
+    return pruned_candidates
+
+# Generate frequent itemsets of size k
+def get_frequent_k_itemsets(data, prev_frequent_itemsets, k, min_support):
+    candidates = generate_candidates(prev_frequent_itemsets, k)
+    pruned_candidates = prune_candidates(candidates, prev_frequent_itemsets, k)
+    item_counts = {tuple(itemset): 0 for itemset in pruned_candidates}
+
+    for transaction in data:
+        for candidate in pruned_candidates:
+            if set(candidate).issubset(transaction):
+                item_counts[candidate] += 1
+
+    frequent_k_itemsets = {itemset: support for itemset, support in item_counts.items() if support >= min_support}
+    return frequent_k_itemsets
+
+# Generate association rules from frequent itemsets
+def generate_association_rules(frequent_itemsets, min_confidence):
+    rules = []
+    for itemset, support in frequent_itemsets.items():
+        if len(itemset) >= 2:
+            for i in range(1, len(itemset)):
+                antecedent = tuple(sorted(list(itemset)[:i]))
+                consequent = tuple(sorted(list(itemset)[i:]))
+                confidence = support / frequent_itemsets[antecedent]
+                if confidence >= min_confidence:
+                    rules.append((antecedent, consequent, confidence))
+    return rules
+
+# Main Apriori algorithm
+def apriori(data, min_support, min_confidence):
+    frequent_itemsets = {}
+    k = 1
+
+    frequent_1_itemsets = get_frequent_1_itemsets(data, min_support)
+    frequent_itemsets.update(frequent_1_itemsets)
+
+    while frequent_itemsets:
+        k += 1
+        frequent_k_itemsets = get_frequent_k_itemsets(data, frequent_itemsets.keys(), k, min_support)
+        if frequent_k_itemsets:
+            frequent_itemsets.update(frequent_k_itemsets)
+
+    association_rules = generate_association_rules(frequent_itemsets, min_confidence)
+    return frequent_itemsets, association_rules
+
+# Load the dataset
+data = load_data('Market_Basket_Optimisation.csv')
+
+# Set minimum support and confidence thresholds
+min_support = 0.01
+min_confidence = 0.3
+
+# Run Apriori algorithm
+frequent_itemsets, association_rules = apriori(data, min_support, min_confidence)
+
+# Display frequent itemsets
+print("Frequent Itemsets:")
+for itemset, support in frequent_itemsets.items():
+    print(f"{itemset}: {support}")
+
+# Display association rules
+print("\nAssociation Rules:")
+for rule in association_rules:
+    antecedent, consequent, confidence = rule
+    print(f"{antecedent} => {consequent} (Confidence: {confidence})")
+
+#
+# from mlxtend.frequent_patterns import apriori
+# from mlxtend.frequent_patterns import association_rules
+# import pandas as pd
+
+# # Load your data from a CSV file
+# df = pd.read_csv('Market_Basket_Optimisation.csv', header=None)
+
+# # Convert the data to a list of transactions
+# transactions = []
+# for index, row in df.iterrows():
+#     transactions.append([str(item) for item in row if pd.notna(item)])
+
+# # Encode the data into a one-hot format
+# oht = pd.get_dummies(pd.DataFrame(transactions), prefix='', prefix_sep='')
+
+# # Use Apriori to find frequent itemsets with a minimum support of 0.2
+# frequent_itemsets = apriori(oht, min_support=0.001, use_colnames=True)  # Adjust the min_support value
+
+
+# # Generate association rules with a minimum confidence of 0.7
+# rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=0.8)
+
+# # Display frequent itemsets and association rules
+# print("Frequent Itemsets:")
+# print(frequent_itemsets)
+
+# print("\nAssociation Rules:")
+# print(rules)
+
+
+
+
+
---
+++ apriori4.py
@@ -0,0 +1,93 @@
+import csv
+from collections import defaultdict
+from itertools import combinations
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+# Load data from CSV
+def load_data(filename):
+    with open(filename, 'r') as f:
+        reader = csv.reader(f)
+        data = list(reader)
+    return data
+
+# Generate candidate itemsets
+def generate_candidate_itemsets(level_k, level_frequent_itemsets):
+    return {itemset1.union(itemset2) for itemset1 in level_frequent_itemsets for itemset2 in level_frequent_itemsets 
+            if len(itemset1.union(itemset2)) == level_k}
+
+# Returns frequent itemsets
+def generate_frequent_itemsets(dataset, candidate_itemsets):
+    counter = defaultdict(int)
+    for transaction in dataset:
+        for itemset in candidate_itemsets:
+            if itemset.issubset(transaction):
+                counter[itemset] += 1
+    return {itemset: frequency for itemset, frequency in counter.items() if frequency/len(dataset) >= min_support}
+
+# Generate association rules
+def generate_rules(frequent_itemsets, min_confidence):
+    rules = []
+    for itemset, frequency in frequent_itemsets.items():
+        for i in range(1, len(itemset)):
+            for subset in combinations(itemset, i):
+                subset = frozenset(subset)
+                if subset in frequent_itemsets:
+                    subset_frequency = frequent_itemsets[subset]
+                    confidence = frequency / subset_frequency
+                    if confidence >= min_confidence:
+                        rules.append((set(subset), itemset.difference(subset), confidence))
+    return rules
+
+# Parameters
+min_support = 0.02
+min_confidence = 0.3
+
+# Load data
+dataset = load_data('Market_Basket_Optimisation.csv')
+
+# Apriori algorithm
+frequent_itemsets = dict()
+for k in range(1, 20):
+    if k == 1:
+        candidate_itemsets = {frozenset([item]) for transaction in dataset for item in transaction}
+    else:
+        candidate_itemsets = generate_candidate_itemsets(k, frequent_itemsets[k-1])
+    frequent_itemsets[k] = generate_frequent_itemsets(dataset, candidate_itemsets)
+    if not frequent_itemsets[k]:
+        del frequent_itemsets[k]
+        break
+
+# Print the frequent itemsets
+for level, itemsets in frequent_itemsets.items():
+    print(f"Level {level}:")
+    for itemset, frequency in itemsets.items():
+        support = frequency / len(dataset)
+        print(f"Itemset: {itemset}, Support: {support}")
+
+# Flatten the frequent_itemsets dictionary
+flat_frequent_itemsets = {itemset: frequency for level_itemsets in frequent_itemsets.values() for itemset, frequency in level_itemsets.items()}
+
+# Generate association rules
+rules = generate_rules(flat_frequent_itemsets, min_confidence)
+# Print the generated rules
+for rule in rules:
+    print(f"{rule[0]} => {rule[1]}, confidence = {rule[2]}")
+    
+
+# # Prepare data for the heatmap
+# antecedents = [str(rule[0]) for rule in rules]
+# consequents = [str(rule[1]) for rule in rules]
+# confidences = [rule[2] for rule in rules]
+# data = pd.DataFrame({'antecedent': antecedents, 'consequent': consequents, 'confidence': confidences})
+
+# # Pivot the data
+# pivot_table = data.pivot(index='antecedent', columns='consequent', values='confidence')
+
+# # Create heatmap
+# plt.figure(figsize=(10, 10))
+# sns.heatmap(pivot_table, cmap='YlGnBu')
+# plt.title('Heatmap of Association Rules')
+# plt.show()
+
